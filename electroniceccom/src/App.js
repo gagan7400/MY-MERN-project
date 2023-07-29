37e28d014b7e37828cc8c1485a91dbc987b7f5b9
Gagan <vishwakarmagagan24@gmail.com>
2023-07-29 12:54:57 +0530

    navbar created by me
    navabr

--- electroniceccom/src/App.js
+++ electroniceccom/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.css'
 function App() {
   return (
     <div className="App">
