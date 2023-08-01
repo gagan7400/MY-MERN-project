b2842b921fa8a93bd6653c8286c0e8bb26466f0d
2211deepstiwari <140878550+2211deepstiwari@users.noreply.github.com>
2023-08-01 10:45:20 +0530

    footer style
    footer style tiondescrip

---
+++ electroniceccom/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Footerstyles from './components/Footerstyles';
+function App() {
+  return (
+    <>
+     <Footer />
+     <Footerstyles/>
+  </>
+  );
+}
+  
+export default App;
